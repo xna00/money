18de21fd096473e47e73aa24e8392d4723bb680e
xna <xna00@qq.com>
2021-01-06 20:59:26 +0800

    Money 组件的 CategorySection UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -41,7 +41,27 @@ const NotesSection = styled.section`
   }
 `
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  > ul{
+    display:flex;
+    background:#c4c4c4;
+    > li {
+      width: 50%; 
+      text-align:center;
+      padding: 16px 0;
+      position:relative;
+      &:nth-child(1)::after{
+        content: '';
+        display:block; 
+        height: 3px;
+        background:#333;
+        position:absolute;
+        bottom:0;
+        width: 100%;
+        left: 0;
+      }
+    }
+  }
 `
 const NumberPadSection = styled.section`
 
