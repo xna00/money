fec1f18ff262fdca3accb0d37629df614b3c82a1
xna <xna00@qq.com>
2021-01-09 21:23:19 +0800

    添加 Toast 组件

---
+++ src/compontents/Toast.tsx
@@ -0,0 +1,62 @@
+import styled from "styled-components";
+import React, {PropsWithChildren, useEffect, useRef, useState} from "react";
+import ReactDOM from 'react-dom';
+
+const Wrapper = styled.div`
+@keyframes fade-in {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+};
+@keyframes fade-out {
+from{
+opacity: 1;
+}
+to{
+opacity: 0;
+}
+}
+animation: fade-in ease-in-out .5s forwards;
+position: absolute;
+bottom: 60px;
+left: 50%;
+transform: translateX(-50%);
+padding: .8em 2em;
+background: rgba(10,10,10,0.5);
+border-radius: 0.4em;
+`
+type Props = {
+    visible: boolean
+} & PropsWithChildren<{}>
+const Toast: React.FC<Props> = (props) => {
+    const wrapperRef = useRef<HTMLDivElement>(null)
+    // setTimeout(() => wrapperRef.current!.style.animationName = 'fade-out', props.showTime + 500)
+    useEffect(() => {
+        !props.visible && (wrapperRef.current!.style.animationName = 'fade-out')
+    }, [props.visible])
+    return (
+        <Wrapper ref={wrapperRef}>
+            {props.children}
+        </Wrapper>
+    )
+}
+const showToast = (content: JSX.Element | string, showTime: number = 1000) => {
+    showTime = showTime || 3000
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const ToastWrapper: React.FC<{ showTime: number } & PropsWithChildren<{}>> = (props) => {
+        const [visible, setVisible] = useState<boolean>(true)
+        setTimeout(() => setVisible(false), showTime)
+        return (
+            <Toast visible={visible}>{props.children}</Toast>
+        )
+    }
+    ReactDOM.render(
+        <ToastWrapper showTime={showTime}>{content}</ToastWrapper>
+        , div)
+    setTimeout(() => div.remove(), showTime + 500 * 3)
+}
+export {showToast}
