c3821795505d723e00c48b81f70130d823efa611
xna <xna00@qq.com>
2021-01-10 00:49:26 +0800

    添加 Chart 组件

---
+++ src/compontents/Chart.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import React, {useEffect, useRef} from "react";
+import {ECharts, EChartOption} from "echarts";
+import * as echarts from 'echarts'
+
+const ChartWrapper = styled.div`
+overflow: auto;
+>div {
+height: 300px;
+width: 420%;
+}
+`
+type Props = {
+    option: EChartOption
+}
+const Chart: React.FC<Props> = (props) => {
+    console.log('chart')
+    const chartWrapperRef = useRef<HTMLDivElement>(null)
+    const chartRef = useRef<HTMLDivElement>(null)
+    let chart = useRef<ECharts>(null)
+    useEffect(() => {
+        // @ts-ignore
+        chart.current = echarts.init(chartRef.current!)
+        chartWrapperRef.current!.scrollLeft = 9999
+    }, [])
+    useEffect(() => {
+        if (!chart) return
+        console.log('chart effect')
+        // @ts-ignore
+        chart.current.setOption(props.option)
+    }, [props.option])
+    return (
+        <ChartWrapper ref={chartWrapperRef}>
+            <div ref={chartRef}></div>
+        </ChartWrapper>
+    )
+}
+export {Chart}
